46ad7417a914fd3d572b15fa7eeca795b37030ca
Gregor Scharf <scharfgregor08@gmail.com>
2023-12-24 23:38:14 +0100

    type conversion expansion

--- math.hpp
+++ math.hpp
@@ -4,28 +4,7 @@
 namespace greg{
 
 double round(double value, int precision){
-    if(precision == 0){
-        return value;
-    }
-    else{
-        if(precision < 0){
-            precision *= -1;
-            double factor = 1;
-            for(int i = 0; i < precision; i++){
-                factor *= 10;
-            }
-            return (int)(value/factor)*factor;
-        }
-        if(precision > 0){
-            double factor = 1;
-            for(int i = 0; i < precision; i++){
-                factor *= 10;
-            }
-            return (int)(value*factor)/factor;
-        }
-    }
-    return -1;
-
+    
 }
 }
 
--- type_conver.hpp
+++ type_conver.hpp
@@ -3,9 +3,87 @@
 #ifndef TYPE_CONVER_HPP
 #define TYPE_CONVER_HPP
 
-// TODO: add support for floats and doubles
+// warning: float to char* is not accurate as it pushes the floating point apraximation value to become bigger than it is
+    // example: 123.456 becomes 123.4560012
+    // avoid by using double_to_char() instead
+
+// for int to char*, use long_to_char() instead, works the same way
+
+// no support for negative numbers yet
+// Todo: add support for negative numbers
 
 namespace greg{
+
+greg::Dy_Array<int> double_to_digits(double value){
+    greg::Dy_Array<int> digits;
+    bool is_negative = value < 0;
+    if(is_negative){
+        value *= -1;
+    }
+    int int_part = (int)value;
+    double decimal_part = value - int_part;
+    greg::Dy_Array<int> int_digits;
+    do {
+        int_digits.push_back(int_part % 10);
+        int_part /= 10;
+    } while (int_part != 0);
+
+    for(int i = int_digits.get_length() - 1; i >= 0; i--){
+        digits.push_back(int_digits[i]);
+    }
+
+    digits.push_back(-2); // decimal point
+
+    // push the decimal part
+    for(int i = 0; i < 15; i++){
+        decimal_part *= 10;
+        digits.push_back((int)decimal_part);
+        decimal_part -= (int)decimal_part;
+    }
+
+    if (is_negative) {
+        for (int i = 0; i < digits.get_length(); i++) {
+            digits[i] *= -1;
+        }
+    }
+    return digits;
+}
+
+greg::Dy_Array<int> float_to_digits(float value){
+    greg::Dy_Array<int> digits;
+    bool is_negative = value < 0;
+    if(is_negative){
+        value *= -1;
+    }
+    int int_part = (int)value;
+    float decimal_part = value - int_part;
+    greg::Dy_Array<int> int_digits;
+    do {
+        int_digits.push_back(int_part % 10);
+        int_part /= 10;
+    } while (int_part != 0);
+
+    for(int i = int_digits.get_length() - 1; i >= 0; i--){
+        digits.push_back(int_digits[i]);
+    }
+
+    digits.push_back(-2); // decimal point
+
+    // push the decimal part
+    for(int i = 0; i < 7; i++){
+        decimal_part *= 10;
+        digits.push_back((int)decimal_part);
+        decimal_part -= (int)decimal_part;
+    }
+
+    if (is_negative) {
+        for (int i = 0; i < digits.get_length(); i++) {
+            digits[i] *= -1;
+        }
+    }
+
+    return digits;
+}
 
 greg::Dy_Array<int> long_to_digits(long value) {
     greg::Dy_Array<int> digits;
@@ -32,18 +110,45 @@ greg::Dy_Array<int> long_to_digits(long value) {
 
     return result;
 }
+
 char* long_to_char(long value){
     greg::Dy_Array<int> digits;
     digits = long_to_digits(value);
     char* result;
-    result = new char[digits.get_length()+2];
+    result = new char[digits.get_length()+1];
     for(int i = 0; i < digits.get_length(); i++){
         result[i] = (digits[i] + 48);
     }
     result[digits.get_length()] = '\0';
     return result;
 }
+
+char* double_to_char(double value){
+    greg::Dy_Array<int> digits;
+    digits = double_to_digits(value);
+    char* result;
+    result = new char[digits.get_length()+1];
+    for(int i = 0; i < digits.get_length(); i++){
+        result[i] = (digits[i] + 48);
+    }
+    result[digits.get_length()] = '\0';
+    return result;
 }
+
+char* float_to_char(float value){
+    greg::Dy_Array<int> digits;
+    digits = float_to_digits(value);
+    char* result;
+    result = new char[digits.get_length()+1];
+    for(int i = 0; i < digits.get_length(); i++){
+        result[i] = (digits[i] + 48);
+    }
+    result[digits.get_length()] = '\0';
+    return result;
+}
+
+}
+
 
 
 
